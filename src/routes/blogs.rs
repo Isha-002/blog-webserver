a17e25a952ea010d9c36bcbc0a6fcfc3f579f7df
Isha-002 <narges1353zeinali@gmail.com>
2025-01-14 00:46:10 +0330

    impl put and del methods - fixed /blogs params bugs

--- src/routes/blogs.rs
+++ src/routes/blogs.rs
@@ -20,27 +20,41 @@ pub async fn blogs(
     State(store): State<Arc<Store>>,
     Query(params): Query<BlogParams>,
 ) -> Result<Json<Vec<Blog>>, (StatusCode, String)> {
-    println!("blog params: {params:?}");
     let res = (*store.posts).read().await.to_vec();
     if let (Some(start), Some(end)) = (params.start, params.end) {
-        let sliced_res = res[start..end].to_vec();
-        Ok(Json(sliced_res))
-    } else {
-        if params.start.is_none() || params.end.is_none() {
+        if end > res.len() {
             return Err((
                 StatusCode::UNPROCESSABLE_ENTITY,
-                "missing params".to_string(),
+                "end parameter is out of bounds".to_string(),
             ));
         }
+        let sliced_res = res[start..end].to_vec();
+        Ok(Json(sliced_res))
+    } else if params.start.is_none() && params.end.is_none() {
         Ok(Json(res))
+    } else if params.end.is_none() && params.start.is_some_and(|s| s < res.len()) {
+        let Some(start) = params.start else { panic!() };
+        Ok(Json(res[start..].to_vec()))
+    } else {
+        return Err((
+            StatusCode::UNPROCESSABLE_ENTITY,
+            "missing params or out of bounds".to_string(),
+        ));
     }
 }
 
 pub async fn single_blog(
     State(store): State<Arc<Store>>,
     Path(blog_id): Path<usize>,
-) -> Json<Blog> {
-    Json(store.posts.read().await[blog_id].clone())
+) -> Result<Json<Blog>, (StatusCode, String)> {
+    let res = store.posts.read().await;
+    if blog_id == 0 || blog_id > res.len() {
+        return Err((
+            StatusCode::NOT_FOUND,
+            "couldnt find block".to_string(),
+        ));
+    }
+    Ok(Json(res[blog_id - 1].clone()))
 }
 
 pub async fn post_blog(State(store): State<Arc<Store>>, Json(payload): Json<Blog>) -> StatusCode {
@@ -48,6 +62,34 @@ pub async fn post_blog(State(store): State<Arc<Store>>, Json(payload): Json<Blog
     StatusCode::CREATED
 }
 
-pub async fn put_blog(State(store): State<Arc<Store>>) {}
+pub async fn put_blog(
+    State(store): State<Arc<Store>>,
+    Path(blog_id): Path<usize>,
+    Json(payload): Json<Blog>,
+) -> Result<StatusCode, (StatusCode, String)> {
+    let mut res = store.posts.write().await;
+    if blog_id == 0 || blog_id > res.len() {
+        return Err((
+            StatusCode::UNPROCESSABLE_ENTITY,
+            "path is not acceptable".to_string(),
+        ));
+    }
+    res[blog_id - 1] = payload;
+    Ok(StatusCode::OK)
+}
 
-pub async fn delete_blog() {}
+pub async fn delete_blog(
+    State(store): State<Arc<Store>>,
+    Path(blog_id): Path<usize>,
+) -> Result<StatusCode, (StatusCode, String)> {
+    let mut res = store.posts.write().await;
+
+    if blog_id == 0 || blog_id > res.len() {
+        return Err((
+            StatusCode::BAD_REQUEST,
+            "blog not found".to_string(),
+        ));
+    }
+    res.remove(blog_id - 1);
+    Ok(StatusCode::NO_CONTENT)
+}
